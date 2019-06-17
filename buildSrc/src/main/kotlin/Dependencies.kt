42e291fa50c222872da14e4b53b47e19d402b7c1
Travis Tong <travis.tong@innopage.com>
2019-06-17 16:19:53 +0800

    Update some dependency w/ naming

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,15 +1,15 @@
 object Jetpack {
     const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
-    const val core = "androidx.core:core-ktx:${Versions.androidXCore}"
-    const val fragment = "androidx.fragment:fragment-ktx:${Versions.fragmentKtx}"
+    const val core = "androidx.core:core-ktx:${Versions.core}"
+    const val fragment = "androidx.fragment:fragment-ktx:${Versions.fragment}"
     // ktx transitively depends on lifecycle, so include only one
     const val lifecycle = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
     const val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerView}"
 
     private object Versions {
-        const val androidXCore = "1.1.0-beta01"
-        const val appCompat = "1.1.0-alpha05"
-        const val fragmentKtx = "1.0.0"
+        const val core = "1.1.0-rc01"
+        const val appCompat = "1.1.0-beta01"
+        const val fragment = "1.1.0-beta01"
         const val lifecycle = "2.2.0-alpha01"
         const val recyclerView = "1.0.0"
     }
@@ -32,7 +32,7 @@ object Kotlin {
         "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.kotlinCoroutines}"
 
     private object Versions {
-        const val kotlin = "1.3.21"
+        const val kotlin = "1.3.31"
         const val kotlinCoroutines = "1.1.1"
     }
 }
@@ -44,7 +44,7 @@ object Testing {
 
     private object Versions {
         const val jUnit = "4.12"
-        const val runner = "1.1.2-alpha01"
-        const val espressoCore = "3.1.2-alpha01"
+        const val runner = "1.2.0"
+        const val espressoCore = "3.2.0"
     }
 }
