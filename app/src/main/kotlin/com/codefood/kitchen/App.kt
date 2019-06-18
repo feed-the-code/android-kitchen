65f8c9af94e927dc7ea67775dca74c194e337517
Travis Tong <travis.tong@innopage.com>
2019-06-18 11:31:13 +0800

    Start koin with network in app

---
+++ app/src/main/kotlin/com/codefood/kitchen/App.kt
@@ -0,0 +1,14 @@
+package com.codefood.kitchen
+
+import android.app.Application
+import com.codefood.network.networkModule
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            modules(listOf(networkModule))
+        }
+    }
+}
