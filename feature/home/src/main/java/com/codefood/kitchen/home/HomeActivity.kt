bda53ff16a8576a01594495e8872fbb2b6af0e79
vis959 <travistkg@gmail.com>
2019-06-26 18:14:42 +0800

    Rename MainActivity -> HomeActivity

--- feature/home/src/main/java/com/codefood/kitchen/home/HomeActivity.kt
+++ feature/home/src/main/java/com/codefood/kitchen/home/HomeActivity.kt
@@ -2,13 +2,13 @@ package com.codefood.kitchen.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_home.*
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
         button.setOnClickListener {
             EmptyBottomSheet().show(supportFragmentManager, "empty_sheet")
         }
